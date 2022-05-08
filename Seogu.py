d7368aefd230e6ba2ccf59a93ee1ee7987377e1d
hjinshin <gudwls818@knu.ac.kr>
2022-05-08 21:06:37 +0900

    mark_Seogu

--- Seogu.py
+++ Seogu.py
@@ -19,12 +19,15 @@ for div in divs:
     for box in boxes:
         #td style="border-width: 0px 0.5pt 0.5pt 0px; border-style: none solid solid none; border-color: black windowtext windowtext black; background-color: transparent;" 추출
         temps = box.findAll('td', {"style": "border-width: 0px 0.5pt 0.5pt 0px; border-style: none solid solid none; border-color: black windowtext windowtext black; background-color: transparent;"})
+
+        #표 1칸씩 infos 리스트에 저장
         infos = []
         for temp in temps:
             lines = temp.findAll('span', {"style": "font-family: 맑은 고딕; font-size: 10pt;"})
 
             #필요한 정보는 lines[0]
             for line in lines:
+
                 if lines.index(line) % 2 == 0:
                     ads = line
                     for ad in ads:
@@ -49,13 +52,14 @@ for div in divs:
 address_json = []
 for cnt in addresses:
     dic = {}
-    dic['위치'] = cnt
+    dic['도로명'] = cnt
     address_json.append(dic)
 
 
 body = json.dumps(address_json, ensure_ascii=False)
 
-pprint(body)
+
+#pprint(body)
 with open('AirPump.txt','w',encoding='UTF-8') as f:
     for i in addresses:
         f.write( i +'\n')
